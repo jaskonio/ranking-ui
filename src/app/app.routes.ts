8b5ab32bcf3a6e78d7af44de7e83cbd954a837f7
Jonatan lopez <jonathan.lp17@gmail.com>
2024-04-23 17:54:24 +0200

    Added layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+import { LeagueComponent } from './page/league.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: AppLayoutComponent,
+    children: [
+      { path: 'ligas', component: LeagueComponent }
+    ]
+  }
+];
---
+++ src/app/page/league.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-league',
+  standalone: true,
+  imports: [],
+  template: '<h1>hola</h1>',
+})
+export class LeagueComponent {
+  title = 'ranking-ui';
+}
